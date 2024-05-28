8d1453c496784bb7b239d7d39859bbd2e6fc765c
FIEF-Nohell <noel.hmnn@gmail.com>
2024-05-28 15:38:03 +0200

    🛠️refactor(components) optimize card size and scroll styling

--- src/app/components/cardHand.tsx
+++ src/app/components/cardHand.tsx
@@ -24,18 +24,18 @@ const Card: React.FC<CardData> = ({ suit, text, bgColor, revealed }) => (
     <>
         {revealed ? (
             <motion.div
-                className={`relative w-40 h-56 ${bgColor} text-tvxt rounded-2xl flex flex-col justify-between p-2 m-1 transform transition-transform duration-300 ease-in-out hover:scale-105`}
+                className={`relative flex-shrink-0 w-24 h-36 sm:w-28 sm:h-40 md:w-32 md:h-44 lg:w-36 lg:h-48 xl:w-40 xl:h-56 ${bgColor} text-tvxt rounded-2xl flex flex-col justify-between p-2 m-1 transform transition-transform duration-300 ease-in-out hover:scale-105`}
                 initial="hidden"
                 animate="visible"
                 variants={cardVariants}
                 transition={{ duration: 0.25 }}
             >
-                <span className="text-[2.5rem] font-bold ml-1">{text}</span>
-                <span className="text-[3rem] font-bold ml-1 text-right mr-2">{suit}</span>
+                <span className="text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] font-bold ml-1">{text}</span>
+                <span className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] font-bold ml-1 text-right mr-2">{suit}</span>
             </motion.div>
         ) : (
             <motion.div
-                className="relative w-40 h-56 bg-[#ffffff3e] rounded-2xl flex flex-col justify-between p-2 m-1 transform transition-transform duration-300 ease-in-out hover:scale-105"
+                className="relative flex-shrink-0 w-24 h-36 sm:w-28 sm:h-40 md:w-32 md:h-44 lg:w-36 lg:h-48 xl:w-40 xl:h-56 bg-[#ffffff3e] rounded-2xl flex flex-col justify-between p-2 m-1 transform transition-transform duration-300 ease-in-out hover:scale-105"
                 initial="hidden"
                 animate="visible"
                 variants={cardVariants}
@@ -47,7 +47,14 @@ const Card: React.FC<CardData> = ({ suit, text, bgColor, revealed }) => (
 
 // CardHand component with props typed
 const CardHand: React.FC<CardHandProps> = ({ cards }) => (
-    <motion.div className="flex items-center justify-center h-full">
+    <motion.div className="flex items-center justify-center h-full" style={{ overflowX: 'scroll', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+        <style>
+            {`
+            ::-webkit-scrollbar {
+                display: none;
+            }
+            `}
+        </style>
         {cards.map((card, index) => (
             <Card key={index} {...card} />
         ))}
