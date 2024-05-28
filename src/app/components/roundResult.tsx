893086afbae22f822be2b0c7d35b35ff7f62285f
FIEF-Nohell <noel.hmnn@gmail.com>
2024-05-28 15:59:56 +0200

    🚀feat(deps) add js-confetti package 🚀feat(components) add confetti animation for Player Wins 🛠️refactor(components) improve code readability in RoundResult component

--- src/app/components/roundResult.tsx
+++ src/app/components/roundResult.tsx
@@ -1,23 +1,42 @@
+import { useEffect } from 'react';
 import { AnimatePresence, motion } from "framer-motion";
+import JSConfetti from 'js-confetti';
+
 export interface RoundResultProps {
     showResult: boolean;
     winner: string;
 }
 
 export default function RoundResult({ showResult, winner }: RoundResultProps) {
+    useEffect(() => {
+        if (showResult && winner == "Player Wins") {
+            const jsConfetti = new JSConfetti();
+            jsConfetti.addConfetti({
+                emojis: ['💸']
+            });
+        }
+    }, [showResult]);
+
     return (
         <AnimatePresence>
-            {showResult ?
+            {showResult ? (
                 <>
-                    <motion.p className="z-50 absolute text-[4rem] font-extrabold"
+                    <motion.p
+                        className="z-50 absolute text-[4rem] font-extrabold"
                         initial={{ x: -2000 }}
                         animate={{ x: 0 }}
-                        exit={{ x: -2000 }}>{winner}</motion.p>
-                    <motion.div className="z-40 absolute w-full h-full bg-blvck/70"
+                        exit={{ x: -2000 }}
+                    >
+                        {winner}
+                    </motion.p>
+                    <motion.div
+                        className="z-40 absolute w-full h-full bg-blvck/70"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}></motion.div>
-                </> : null}
+                        exit={{ opacity: 0 }}
+                    />
+                </>
+            ) : null}
         </AnimatePresence>
-    )
+    );
 }
